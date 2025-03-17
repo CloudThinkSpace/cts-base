00cf31db43766bef67de63ba6d73fb566a9a4653
飘香随云 <wozaifeiyang0@163.com>
2025-03-17 19:44:00 +0800

    csv 重构转换

--- cts-sql-expression/src/convert/csv.rs
+++ cts-sql-expression/src/convert/csv.rs
@@ -10,7 +10,7 @@ impl PgRowConvert for CsvConvert {
         let mut result = Vec::new();
         for row in data.into_iter() {
             let vec_pg = SerVecPgRow::from(row);
-            let value = vec_pg.into();
+            let value:Value = vec_pg.into();
             result.push(value)
         }
         Value::Array(result)

2c059b6201d8389a164b3eb2489ee634222b2c06
飘香随云 <wozaifeiyang0@163.com>
2025-03-19 22:46:26 +0800

    错误类型实现display trait

--- cts-upload/src/error.rs
+++ cts-upload/src/error.rs
@@ -3,3 +3,16 @@ pub enum CtsUpLoadError {
     ReadError(String),
     WriteError(String),
 }
+
+impl std::fmt::Display for CtsUpLoadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            CtsUpLoadError::ReadError(data) => {
+                write!(f, "{}", data)
+            }
+            CtsUpLoadError::WriteError(data) => {
+                write!(f, "{}", data)
+            }
+        }
+    }
+}
